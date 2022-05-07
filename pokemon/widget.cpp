fad973f6a57625ea1571a47f9e3c8066629fdbb5
Teddy <38688767+teddyvltn@users.noreply.github.com>
2022-05-07 22:56:56 +0200

    test

--- pokemon/widget.cpp
+++ pokemon/widget.cpp
@@ -1,11 +1,17 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#include "utils.h"
+
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    auto pokemons = extract_fileData("pokemon.txt");
+    auto type = extract_fileData("type.txt");
+    print_data(pokemons);
 }
 
 Widget::~Widget()
